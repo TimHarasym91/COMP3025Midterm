35904699f02d690fb1a3c3e1df926709cd9689cc
Tim Harasym <timharasym@Tims-MacBook-Pro.local>
2017-02-22 10:05:02 -0500

    Functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,26 +4,34 @@ import { MyApp } from './app.component';
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
+import { AngularFireModule } from 'angularfire2';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyB7YVs53EK4PNyuf2fAiDLOytkVLW1wXr4",
+  authDomain: "shopplinglistmidterm.firebaseapp.com",
+  databaseURL: "https://shopplinglistmidterm.firebaseio.com",
+  storageBucket: "shopplinglistmidterm.appspot.com",
+  messagingSenderId: "507532900587"
+}
 
 @NgModule({
   declarations: [
     MyApp,
     AboutPage,
     ContactPage,
-    HomePage,
-    TabsPage
+    HomePage
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     AboutPage,
     ContactPage,
-    HomePage,
-    TabsPage
+    HomePage
+
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,114 @@
 import { Component } from '@angular/core';
 
-import { NavController } from 'ionic-angular';
+import {AngularFire, FirebaseListObservable} from 'angularfire2';
+
+import {
+  NavController,
+  AlertController,
+  ActionSheetController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  items: FirebaseListObservable<any>;
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController, af: AngularFire, public actionSheetCtrl: ActionSheetController) {
+    this.items = af.database.list('/items');
   }
+
+// Add new item to the list
+  addItem(){
+    let prompt = this.alertCtrl.create({
+      title: 'Item',
+      message: "Enter an item",
+      inputs: [
+        {
+          name: 'title',
+          placeholder: 'Title'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            this.items.push({
+              title: data.title,
+              status: false,
+              amount: 1
+            });
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
+
+  // Delete a specific item from the list
+  removeItem(itemId: string){
+    this.items.remove(itemId);
+  }
+
+// Increase the amount on an item
+increaseItem(itemId, itemAmount){
+  itemAmount++;
+  this.items.update(itemId,{
+    amount: itemAmount
+  });
+}
+
+//Decrease the amount on an item
+decreaseItem(itemId, itemAmount){
+  itemAmount--;
+  if(itemAmount <= 0 ){
+    itemAmount = 0;
+  }
+  this.items.update(itemId,{
+    amount: itemAmount
+  });
+}
+
+updateStatus(itemId, itemStatus){
+  if(itemStatus == false){
+    this.items.update(itemId,{
+      status: true
+    });
+  }
+  else if(itemStatus == true){
+    this.items.update(itemId,{
+      status: false
+    });
+  }
+}
+
+//Clear the entire List
+clearList(){
+  let prompt = this.alertCtrl.create({
+    title: 'Clear List',
+    message: "Are you sure you wish to delete this list?",
+    buttons: [
+      {
+        text: 'Cancel',
+        handler: data => {
+        console.log('Cancel clicked');
+        }
+      },
+      {
+        text: 'Clear',
+        handler: data => {
+          this.items.remove();
+        }
+      }
+    ]
+  });
+  prompt.present();
+}
 
 }
